5218b08aca177d1428a4467ad0abd5dd2e945836
Hajtó Kornél <72157420+Lxndr01@users.noreply.github.com>
2022-09-12 22:13:24 +0200

    Fixed some stuff.
    Fixed the new characters animation system.

---
+++ WitchyGame/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        Vector2 horizontalmove = transform.right * movement.x;
+        Vector2 verticalmove = transform.up * movement.y;
+        Vector2 velocity = (horizontalmove + verticalmove).normalized * moveSpeed;
+
+        //animation
+
+
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    /*void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity *Time.fixedDeltaTime);
+    }*/
+
+}
