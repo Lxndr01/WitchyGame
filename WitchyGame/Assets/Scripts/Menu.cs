537467305e973fc4e8be698d4e09d77468d5690c
BojszaReka <113019065+BojszaReka@users.noreply.github.com>
2022-09-16 20:39:33 +0200

    UI prefab

--- WitchyGame/Assets/Scripts/Menu.cs
+++ WitchyGame/Assets/Scripts/Menu.cs
@@ -8,7 +8,7 @@ public class Menu : MonoBehaviour
     public void PlayGame()
     {
         Debug.Log("Start");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void Quit()
